e230fa38ee80a7f8ab78fbc4fb1abd0b4b3b4446
Volodymyr Basok <volodymyrquo@gmail.com>
2025-01-12 15:02:38 +0200

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,18 @@
+import { sampleBooks } from '@/constants';
+
+import BookList from '@/components/BookList';
+import BookOverview from '@/components/BookOverview';
+
+const Home = () => {
+    return (
+        <>
+            <BookOverview {...sampleBooks[0]} />
+            <BookList
+                title="Latest Books"
+                books={sampleBooks}
+                containerClassName="mt-28"
+            />
+        </>
+    );
+};
+export default Home;
